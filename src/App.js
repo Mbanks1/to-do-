0dae8fa60738d8b3692cbe612fcad83d66b9ae13
Mbanks1 <michael1banks@icloud.com>
2021-01-19 22:47:05 -0400

    app changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+  super(props);
+
+  this.state={
+    newItems: "",
+    list: []
+  }
+  }
   
-  state = {  }
   render() { 
     return ( 
       <> 
